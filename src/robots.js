49518b2b90070f60d59f926a8d6720aab1f5246e
Tomas Ramirez <ramirez.tomas@yahoo.com>
2020-10-27 21:53:24 -0700

    RoboFriends Update

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Connor McGregor',
+        username: 'ProperW',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 3,
+        name: 'Derek Jeter',
+        username: 'Dos',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 4,
+        name: 'Fransico Rodriguez',
+        username: 'KROD',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 5,
+        name: 'Floyd Mayweather',
+        username: 'TBE',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 6,
+        name: 'Dak Prescott',
+        username: 'D4',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 7,
+        name: 'Jimmy Butler',
+        username: 'Buckets',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 8,
+        name: 'Mike Tyson',
+        username: 'Iron',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 9,
+        name: 'Joey Chestnut',
+        username: 'SJSU',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 10,
+        name: 'Juan Diaz',
+        username: 'BabyBull',
+        email: 'Sincere@april.biz'
+    }
+]
