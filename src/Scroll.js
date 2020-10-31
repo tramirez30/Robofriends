7fd5e3878c2e29c750806119a63b4c5f60db131c
Tomas Ramirez <ramirez.tomas@yahoo.com>
2020-10-30 22:48:34 -0700

    Updated scroll

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: 'scroll', border: '1px solid black', height: '500px'}}>
+        <div style={{ overflow: 'scroll', border: '2px solid black', height: '500px'}}>
             {props.children}
         </div>
     );
